023eceb104fd9e3be372f06343313c6c1d95ba85
railken <railken@protonmail.ch>
2019-07-14 13:08:30 +0200

    fix style

--- src/Managers/EmailSubscriptionManager.php
+++ src/Managers/EmailSubscriptionManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\EmailSubscription newEntity()
- * @method \Amethyst\Schemas\EmailSubscriptionSchema getSchema()
+ * @method \Amethyst\Models\EmailSubscription                 newEntity()
+ * @method \Amethyst\Schemas\EmailSubscriptionSchema          getSchema()
  * @method \Amethyst\Repositories\EmailSubscriptionRepository getRepository()
- * @method \Amethyst\Serializers\EmailSubscriptionSerializer getSerializer()
- * @method \Amethyst\Validators\EmailSubscriptionValidator getValidator()
- * @method \Amethyst\Authorizers\EmailSubscriptionAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\EmailSubscriptionSerializer  getSerializer()
+ * @method \Amethyst\Validators\EmailSubscriptionValidator    getValidator()
+ * @method \Amethyst\Authorizers\EmailSubscriptionAuthorizer  getAuthorizer()
  */
 class EmailSubscriptionManager extends Manager
 {
