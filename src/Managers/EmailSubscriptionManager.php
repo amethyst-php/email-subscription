f7f01dfca42b228f6d442a6dcb37451159fc6b89
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/EmailSubscriptionManager.php
+++ src/Managers/EmailSubscriptionManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\EmailSubscription newEntity()
+ * @method \Amethyst\Schemas\EmailSubscriptionSchema getSchema()
+ * @method \Amethyst\Repositories\EmailSubscriptionRepository getRepository()
+ * @method \Amethyst\Serializers\EmailSubscriptionSerializer getSerializer()
+ * @method \Amethyst\Validators\EmailSubscriptionValidator getValidator()
+ * @method \Amethyst\Authorizers\EmailSubscriptionAuthorizer getAuthorizer()
+ */
 class EmailSubscriptionManager extends Manager
 {
     use ConfigurableManager;
